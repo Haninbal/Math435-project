ce69f5f665f395498fb6eb29200a4fab5972322b
Haninbal <42582843+Haninbal@users.noreply.github.com>
2020-04-30 23:33:53 -0600

    fixed some bugs, wrote output to csv

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ import sys
 import circle as cir
 import numpy as np
 
+_last = None
+_filename = None
+
+def setup_output(filename):
+	global _filename
+	
+	with open(filename, "w+") as f:
+		f.write("")
+		_filename = filename
+		
+
 def to_output(point):
-	#pass
-	print(str(point))
+	global _filename
+	
+	with open(_filename, "a") as f:
+		f.write(str(point)[1:-1]+ "\n")
+	
+	
+def set_last(k):
+	global _last 
+	_last = k
+	#print(k)
 	
 def floor(k):
-	if int(k) == k:
-		return k-1
-	else:
-		return int(k)
+	global _last
+	return _last
 	
 def euclidean_distance(x, y, z):
 	return np.sqrt(x**2 + y**2 + z**2)
@@ -23,31 +40,60 @@ def function(y):
 	
 	return [x0, x1, x2, x3]
 	
-def mapping(cirpoint, c, r, n, ep, bound, last, k):
-	y = cirpoint + [int(r)]
-		
+def compute_point(cirpoint, r):
+	y = cirpoint + [r]
+	#print(y)
 	X = function(y)
-	if abs(X[3] > 1e-10):
+	#print(X)
+	if abs(X[3] > 1e-20):
 		x, y, z = X[0]/X[3], X[1]/X[3], X[2]/X[3]
-		if euclidean_distance(x, y, z) < bound:
-			if last is not None: 
-				d = euclidean_distance(x-last[0], y-last[1], z-last[2])
-				if d > ep:
-					nk = (k+floor(k)) / 2
-					'''if abs(nk -k) < 1e-6:
-						break '''
-					#print(str(k)+" "+str(floor(k))+" "+str(nk))
-					k = nk
-					cirpoint = c.eval(nk)
-					last = mapping(cirpoint, c, r, n, ep, bound, last, nk)
-					if last is not None:
-						to_output(last)
-						d = euclidean_distance(x-last[0], y-last[1], z-last[2])
-					'''else:
-						break '''
-			else:
-				d = "n/a"
-			return [x, y, z, d]
+		return [x, y, z]
+	return None
+	
+def interpolate_points(c, r, ep, bound, left_point, right_point, beg_k, mid_k, end_k):	
+	if beg_k == mid_k or beg_k >= end_k:
+		print("max depth reached in interpolate_points, something went wrong: beg_k="+str(beg_k))
+	cirpoint = c.eval(mid_k)
+	p = compute_point(cirpoint, r)
+		
+	x, y, z = p[0], p[1], p[2]
+	if euclidean_distance(x, y, z) < bound:
+		d = euclidean_distance(x-left_point[0], y-left_point[1], z-left_point[2])
+		if d > ep: #add any points that need to be added prior to the point
+			lm_point = interpolate_points(c, r, ep, bound, left_point, p, beg_k, (beg_k+mid_k)/2, mid_k)
+			if lm_point is not None:
+				d = euclidean_distance(x-lm_point[0], y-lm_point[1], z-lm_point[2])
+			
+		to_output(p + [d, mid_k]) #add the point 
+		
+		d = euclidean_distance(x-right_point[0], y-right_point[1], z-right_point[2])
+		rm_point = None
+		if d > ep: #addany points that need to be added after the point 
+			rm_point = interpolate_points(c, r, ep, bound, p, right_point, mid_k, (mid_k+end_k)/2, end_k)
+			
+		return rm_point if rm_point is not None else p
+	else:
+		return
+			
+				
+	
+def mapping(cirpoint, c, r, n, ep, bound, last, k):
+	p = compute_point(cirpoint, r)
+	if p == None:
+		return
+	x, y, z = p[0], p[1], p[2]
+	
+	if euclidean_distance(x, y, z) < bound:
+		if last is not None: 
+			d = euclidean_distance(x-last[0], y-last[1], z-last[2])
+			if d > ep:
+				r_point = interpolate_points(c, r, ep, bound, last, p, k-1, k-0.5, k)
+				if r_point is not None:
+					d = euclidean_distance(x-r_point[0], y-r_point[1], z-r_point[2])
+		else:
+			d = "n/a"
+		set_last(k)
+		return p + [d]
 	return None
 
 def main(args):
@@ -56,15 +102,34 @@ def main(args):
 	n = int(args[1]) if len(args) >= 2 else 2000
 	ep = float(args[2]) if len(args) >= 3 else 0.06
 	bound = float(args[3]) if len(args) >= 4 else 10
-	c = cir.circle(args[0], args[1])
+	filename = args[4] if len(args) >= 5 else "points.csv"
+	
+	if "test" in args:
+		c = cir.circle(r, n)
+		p = compute_point(c.eval(40), r)
+		print(p)
+		c = cir.circle(0.9, n)
+		p = compute_point(c.eval(40), r)
+		print(p)
+		return
+	
+	print("radius: "+str(r)+" number of points: "+str(n)+" epsilon: "+str(ep)+" bound: "+str(bound))
+	
+	setup_output(filename)
+	
+	c = cir.circle(r, n)
 	last = None
 	
 	
 	for k in range(n):
 		cirpoint = c.eval(k)
-		last = mapping(cirpoint, c, r, n, ep, bound, last, k)
-		if last is not None:
-			to_output(last)
+		temp = mapping(cirpoint, c, r, n, ep, bound, last, k)
+		if temp is not None:
+			to_output(temp+[k])
+			last = temp
+		else:
+			pass
+			#print("input of "+str(k)+" was not in the valid area")
 	
 
 if __name__ == '__main__':
